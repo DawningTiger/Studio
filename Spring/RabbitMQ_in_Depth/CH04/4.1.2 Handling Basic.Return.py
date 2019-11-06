19b0f39ae8c6e7c273a762a96c5bd936407c7d3b
DawningTiger <410758267@qq.com>
2019-11-06 16:45:52 +0800

    Update Spring/RabbitMQ_in_Depth/CH04 to reformat code.

--- Spring/RabbitMQ_in_Depth/CH04/4.1.2 Handling Basic.Return.py
+++ Spring/RabbitMQ_in_Depth/CH04/4.1.2 Handling Basic.Return.py
@@ -5,13 +5,15 @@ import rabbitpy
 connection = rabbitpy.Connection()
 try:
     with connection.channel() as channel:
+        # Create the message to send
+        body = 'server.cpu.utilization 25.5 1350884514'
         properties = {'content_type': 'text/plain',
                       'timestamp': datetime.datetime.now(),
                       'message_type': 'graphite metric'}
-        body = 'server.cpu.utilization 25.5 1350884514'
         message = rabbitpy.Message(channel, body, properties)
-        message.publish('chapter2-example',
-                        'server-metrics',
-                        mandatory=True)
+
+        # Publish the message to the exchange with the routing key
+        # "server-metrics" and make sure it is routed to the exchange
+        message.publish('chapter2-example', 'server-metrics', mandatory=True)
 except rabbitpy.exceptions.MessageReturnedException as error:
     print('Publish failure: %s' % error)
